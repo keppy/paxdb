7e2929048c9f75b3369a8aadedd42a73bbb0024b
keppy <keppy.rb@gmail.com>
2013-10-14 15:17:24 -0700

    Change URL from http to https

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject paxdb "0.1.0"
   :description "Clojure graph data system inspired by the Paxos protocol."
-  :url "http://keppy.github.com/paxdb"
+  :url "https://keppy.github.com/paxdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
