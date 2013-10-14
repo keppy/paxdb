c048b255f2d24b4d16e469e6f120421323f56afa
keppy <keppy.rb@gmail.com>
2013-10-14 14:46:20 -0700

    adds :scm option to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject paxdb "0.1.0"
   :description "Clojure graph data system inspired by the Paxos protocol."
-  :url "http://keppy.github.com"
+  :url "http://keppy.github.com/paxdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [factual/clj-leveldb "0.1.0"]
-                 [byte-streams "0.1.5"]])
+                 [byte-streams "0.1.5"]]
+  :scm {:name "git"
+         :url "https://github.com/keppy/paxdb"})
