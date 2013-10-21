2fd2d4d72e19f65c0a29822ad56cc1a00128333a
keppy <keppy.rb@gmail.com>
2013-10-21 06:24:19 -0700

    project fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject paxdb "0.1.0"
-  :description "Clojure graph data system inspired by the Paxos protocol."
-  :url "https://keppy.github.com/paxdb"
+  :description "Clojure graph data system."
+  :url "https://github.com/keppy/paxdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
