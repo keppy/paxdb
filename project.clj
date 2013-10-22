db6f6982b58bb66f941014bfb64e24f6024572cc
keppy <keppy.rb@gmail.com>
2013-10-22 11:59:21 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject paxdb "0.1.0"
+(defproject org.clojars.keppy/paxdb "0.1.0-SNAPSHOT"
   :description "Clojure graph data system."
   :url "https://github.com/keppy/paxdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [factual/clj-leveldb "0.1.0"]
-                 [byte-streams "0.1.5"]]
-  :scm {:name "git"
-         :url "https://github.com/keppy/paxdb"})
+                 [byte-streams "0.1.5"]])
